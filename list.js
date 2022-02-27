15677adc23edb020ef7a22a54bfbd2811d4e0b40
julikss <julie.lehenka@gmail.com>
2022-02-27 10:49:54 +0200

    Add insert method to list
    It adds new element to the certain position in the list.

--- list.js
+++ list.js
@@ -12,8 +12,8 @@ class CircularList {
     }
 
     //add new element to the end of the list
-    append(element) {
-        const newNode = { element: element, next: this.head };
+    append(data) {
+        const newNode = { element: data, next: this.head };
 
         if (!this.head || !this.tail) {
             this.head = newNode;
@@ -23,8 +23,53 @@ class CircularList {
 
         this.tail.next = newNode;
         this.tail = newNode;
-        this.length +=1;
+        this.length++;
         return newNode;
     }
+
+    //add new element to the certain position in the list
+    insert(data, index) {
+        if (typeof index !== 'number' || index < 0 || index > (this.length + 1)) {
+            throw new Error('You entered invalid index');
+        }
+        
+        const newNode = { element: data, next: this.head };
+        this.length++;
+
+        if (index === 0) {
+            this.head.prev = newNode;
+            newNode.next = this.head;
+            this.head = newNode;
+            return newNode;
+        } else {
+            let counter = 0;
+            let prevNode;
+            let currNode = this.head;
+
+            while (counter < index) {
+                prevNode = currNode;
+                currNode = currNode.next;
+                counter++;
+            }
+
+            prevNode.next = newNode;
+            currNode.prev = newNode;
+            newNode.prev = prevNode;
+            newNode.next = currNode;
+            return newNode;
+        }
+
+    }
+
     
 }
+
+const list = new CircularList();
+list.append('a');
+list.append('b');
+list.append('c');
+list.append('b');
+list.insert('u', 4);
+list.insert('l', 5);
+console.log(list);
+//console.log(list.length);
