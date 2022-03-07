ff1d73326726cdf9902a7bf2a38964649458566c
julikss <julie.lehenka@gmail.com>
2022-03-07 17:08:35 +0200

    Create tests for append()

--- list.test.js
+++ list.test.js
@@ -10,13 +10,38 @@ describe('testing getLength method', () => {
     test('should return length of an empty list (0)', () => {
         const list = new CircularList();
         expect(list.getLength()).toBe(0);
-    })
+    });
 
     test('should return length of a list with one element', () => {
         const list = new CircularList();
         list.append('a');
         expect(list.getLength()).toBe(1);
+    });
+});
+
+
+describe('testing append method', () => {
+    const list = new CircularList();
+    
+    it('should increase length property', () => {
+        list.clear();
+        const startLen = list.length;
+        list.append('a');
+        expect(list.length).toBeGreaterThan(startLen);
+    });
+
+    it('should add element to the end of the list', () => {
+        list.clear();
+        list.append('b');
+        expect(list.get(list.length - 1)).toEqual('b');
+    });
+
+    it('tail should have new value', () => {
+        list.clear();
+        list.append('c');
+        expect(list.tail.element).toEqual('c');
     })
-
-
 })
+
+
+  
